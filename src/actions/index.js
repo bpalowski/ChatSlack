92fd15751271cf5a75592e5b7e5f6ffcd3efd23f
bpalowski <brianpalowski00@gmail.com>
2019-04-30 16:55:04 -0700

    Update Avatar

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,37 +1,56 @@
-import * as actionTypes from './types';
-// USER ACTION
+import * as actionTypes from "./types";
+
+/* User Actions */
 export const setUser = user => {
 	return {
 		type: actionTypes.SET_USER,
 		payload: {
 			currentUser: user
 		}
-	}
-}
+	};
+};
 
-export const clearUser = user => {
+export const clearUser = () => {
 	return {
 		type: actionTypes.CLEAR_USER
-	}
-}
+	};
+};
 
-
-// CHANNEL ACTION
+/* Channel Actions */
 export const setCurrentChannel = channel => {
 	return {
 		type: actionTypes.SET_CURRENT_CHANNEL,
 		payload: {
 			currentChannel: channel
 		}
-	}
-}
+	};
+};
 
-// CHANNEL PRIVATE
 export const setPrivateChannel = isPrivateChannel => {
 	return {
 		type: actionTypes.SET_PRIVATE_CHANNEL,
 		payload: {
 			isPrivateChannel
 		}
-	}
-}
+	};
+};
+
+export const setUserPosts = userPosts => {
+	return {
+		type: actionTypes.SET_USER_POSTS,
+		payload: {
+			userPosts
+		}
+	};
+};
+
+/* Colors Actions */
+export const setColors = (primaryColor, secondaryColor) => {
+	return {
+		type: actionTypes.SET_COLORS,
+		payload: {
+			primaryColor,
+			secondaryColor
+		}
+	};
+};
